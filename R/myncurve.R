68fff00a34a9e6313684cd1508958039487f0989
TheShortPerson <gavin@theshortpeople.com>
2022-02-24 23:42:34 -0600

    commit 2-24-2022

---
+++ R/myncurve.R
@@ -0,0 +1,18 @@
+#' Title MyNCurve
+#'
+#' @param mu Mean of distribution
+#' @param sigma Std. Deviation of Distribution
+#' @param a Requested value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+myncurve = function(mu, sigma, a){
+  curve(dnorm(x, mean = mu, sd = sigma), xlim = c(mu - 3*sigma, mu + 3*sigma), lwd = 3)
+  xcurve = seq(-3*sigma, a, length=1000)
+  ycurve = dnorm(xcurve, mu, sigma)
+  polygon(c(-3*sigma, xcurve, a), c(0, ycurve, 0), col = "Red")
+  prob = round((pnorm(a,mu,sigma)), 4)
+  text(x = a, y = 0.02, paste("Area = ", prob, sep = ""))
+}
