eab89fa42a702cf1946f061d8e5227a3944d46b8
TheShortPerson <gavin@theshortpeople.com>
2022-04-20 10:39:15 -0500

    commit 4-20-2022

---
+++ R/myci.R
@@ -0,0 +1,17 @@
+#' @title MyCIMATH
+#'
+#' @param x sample data
+#'
+#' @return A 95% confidence interval for the mean of the population being sampled
+#' @export
+#'
+#' @examples
+#' \dontrun{myci(x = rnorm(30, mean = 10, sd = 12))}
+myci <- function(x){
+  n = length(x)
+  t = qt(0.975, (n - 1))
+  ci = c()
+  ci[1] = mean(x) - t*sd(x) / sqrt(n)
+  ci[2] = mean(x) + t*sd(x) / sqrt(n)
+  ci
+}
